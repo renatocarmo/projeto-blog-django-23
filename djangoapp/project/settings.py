2e8feb002a3c332538c406176ac477e786ce2039
Renato Carmo <renatocarmo@msn.com>
2023-12-13 23:23:05 -0300

    Add Template Bootstrap

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = DATA_DIR / 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = DATA_DIR / 'media'
 
